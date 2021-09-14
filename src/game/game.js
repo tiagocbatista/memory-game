e8857a9797c813fdca55e35084a0862358159c28
Tiago Batista <tiagocbatista@gmail.com>
2021-09-14 17:28:14 -0300

    Improving game logic and functions

--- src/game/game.js
+++ src/game/game.js
@@ -4,45 +4,6 @@ let game = {
     firstCard: null,
     secondCard: null,
 
-    setCard: function (id) {
-        let card = this.cards.filter(card => card.id === id)[0];
-
-        if (card.flipped || this.lockMode) {
-            return false;
-        }
-
-        if (!this.firstCard) {
-            this.firstCard = card;
-            this.firstCard.flipped = true;
-            return true;
-        } else {
-            this.secondCard = card;
-            this.secondCard.flipped = true;
-            this.lockMode = true;
-            return true;
-        }
-    },
-
-    checkMatch: function () {
-        if (!this.firstCard || !this.secondCard) {
-            return false;
-        }
-        return this.firstCard.icon === this.secondCard.icon;
-
-    },
-
-    unflipCards: function () {
-        this.firstCard.flipped = false;
-        this.secondCard.flipped = false;
-        this.clearCards();
-    },
-
-    clearCards: function () {
-        this.firstCard = null;
-        this.secondCard = null;
-        this.lockMode = false;
-    },
-
     techs: ['bootstrap',
         'css',
         'electron',
@@ -57,49 +18,110 @@ let game = {
 
     cards: null,
 
-    createCardsFromTech: function () {
+    setCard: function (id) {
+
+        let card = this.cards.filter(card => card.id === id)[0];
+        console.log(card);
+        if (card.flipped || this.lockMode) {
+            return false;
+        }
+
+        if (!this.firstCard) {
+            this.firstCard = card;
+            this.firstCard.flipped = true;
+            return true;
+        } else {
+            this.secondCard = card;
+            this.secondCard.flipped = true;
+            this.lockMode = true;
+            return true;
+        }
+
+    },
+
+    checkMatch: function () {
+        if (!this.firstCard || !this.secondCard) {
+            return false;
+        }
+        return this.firstCard.icon === this.secondCard.icon;
+    },
+
+    clearCards: function () {
+        this.firstCard = null;
+        this.secondCard = null;
+        this.lockMode = false;
+    },
+    unflipCards() {
+        this.firstCard.flipped = false;
+        this.secondCard.flipped = false;
+        this.clearCards();
+    },
+
+    checkGameOver() {
+
+        return this.cards.filter(card => !card.flipped).length === 0;
+    },
+
+    createCardsFromTechs: function () {
+
         this.cards = [];
 
-        this.techs.forEach(tech => {
+        this.techs.forEach((tech) => {
             this.cards.push(this.createPairFromTech(tech));
         })
-
         this.cards = this.cards.flatMap(pair => pair);
         this.shuffleCards();
-        return this.cards
+        return this.cards;
     },
 
     createPairFromTech: function (tech) {
+
         return [{
             id: this.createIdWithTech(tech),
             icon: tech,
-            flipped: false
+            flipped: false,
         }, {
             id: this.createIdWithTech(tech),
             icon: tech,
-            flipped: false
+            flipped: false,
         }]
+
     },
 
     createIdWithTech: function (tech) {
         return tech + parseInt(Math.random() * 1000);
     },
 
-    shuffleCards: function () {
+    shuffleCards: function (cards) {
         let currentIndex = this.cards.length;
         let randomIndex = 0;
 
         while (currentIndex !== 0) {
-            let randomIndex = Math.floor(Math.random() * currentIndex);
+
+            randomIndex = Math.floor(Math.random() * currentIndex);
             currentIndex--;
 
-            [this.cards[randomIndex], this.cards[currentIndex]] = [this.cards[currentIndex], this.cards[randomIndex]];
+            [this.cards[randomIndex], this.cards[currentIndex]] = [this.cards[currentIndex], this.cards[randomIndex]]
         }
 
     },
 
-    checkGameOver: function () {
-        return this.cards.filter(card => !card.flipped).length == 0;
+    flipCard: function (cardId, gameOverCallback, noMatchCallback) {
+        if (this.setCard(cardId)) {
+            if (this.secondCard) {
+                if (this.checkMatch()) {
+                    this.clearCards();
+                    if (this.checkGameOver()) {
+                        gameOverCallback();
+                    }
+                } else {
+                    setTimeout(() => {
+                        this.unflipCards();
+                        noMatchCallback();
+                    }, 1000)
+                }
+            }
+        }
     }
 }
 
